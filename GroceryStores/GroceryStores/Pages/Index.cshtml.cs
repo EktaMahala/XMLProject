7fef65ba9947b78be04d9ed05329e5e40153907e
Naga Snigdha Gayatri Ganugapaty <sriushanag@gmail.com>
2019-11-20 14:33:48 -0500

    Changes for Code Review

--- GroceryStores/GroceryStores/Pages/Index.cshtml.cs
+++ GroceryStores/GroceryStores/Pages/Index.cshtml.cs
@@ -8,6 +8,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using BusinessOwnerDetails;
 using Microsoft.Extensions.Logging;
 
+/*Grocery Stores Project Combines two JSON files.
+ * One file named BusinessOwner has the information about business owners and their titles
+ * Another file named GroceryStore has information like address, location, zip etc  
+ * The field common to both of these JSON is Account Number
+ * By matching the Account Number we can determine the owner name and nearby grocery stores and display the information in a table
+ */
 namespace GroceryStores.Pages
 {
     public class IndexModel : PageModel
