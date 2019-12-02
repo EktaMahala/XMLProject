53d2e3240e1435d93ea6375540596f3a42b5e42c
EktaMahala <54607380+EktaMahala@users.noreply.github.com>
2019-12-02 14:07:53 -0500

    Add project files.

---
+++ GroceryStores/Pages/Index.cshtml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BusinessOwnerDetails;
+using Microsoft.Extensions.Logging;
+
+/*Grocery Stores Project Combines two JSON files.
+ * One file named BusinessOwner has the information about business owners and their titles
+ * Another file named GroceryStore has information like address, location, zip etc  
+ * The field common to both of these JSON is Account Number
+ * By matching the Account Number we can determine the owner name and nearby grocery stores and display the information in a table
+ */
+namespace GroceryStores.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string businessOwnerEndPoint = "https://data.cityofchicago.org/resource/r5kz-chrr.json";
+                string BusinessOwnerJsonString = webClient.DownloadString(businessOwnerEndPoint);
+                BusinessOwner[] allBusinessOwners = BusinessOwner.FromJson(BusinessOwnerJsonString);
+                ViewData["allBusinessOwners"] = allBusinessOwners;
+
+                string groceryStoreEndPoint = "https://data.cityofchicago.org/resource/53t8-wyrc.json";
+                string GroceryStoreJsonString = webClient.DownloadString(groceryStoreEndPoint);
+                GroceryStore[] allGroceryStores = GroceryStore.FromJson(GroceryStoreJsonString);
+                ViewData["allGroceryStores"] = allGroceryStores;
+
+                IDictionary<long, GroceryStore> groceryStoresMap = new Dictionary<long, GroceryStore>();
+                List<BusinessOwner> businessOwnerList = new List<BusinessOwner>();
+
+                foreach (GroceryStore grocery in allGroceryStores)
+                {
+                    if (!groceryStoresMap.ContainsKey(grocery.ZipCode))
+                    {
+                        groceryStoresMap.Add(grocery.ZipCode, grocery);
+                    }                    
+                }
+
+
+                foreach (BusinessOwner businessRec in allBusinessOwners)
+                {
+                    if (groceryStoresMap.ContainsKey(businessRec.ZipCode))
+                    {
+                        businessOwnerList.Add(businessRec);
+                    }
+                }
+
+                /*IDictionary<long, BusinessOwner> businessOwnersMap = new Dictionary<long, BusinessOwner>();
+
+                List<GroceryStore> groceryStoresOwners = new List<GroceryStore>();
+
+                foreach(BusinessOwner businessOwner in allBusinessOwners){
+                    businessOwnersMap.Add(businessOwner.LicenseId, businessOwner);
+                }
+
+
+                foreach (GroceryStore groceryStore in allGroceryStores)
+                {
+                    if (businessOwnersMap.ContainsKey(groceryStore.LicenseId))
+                    {
+                        groceryStoresOwners.Add(groceryStore);
+                    }
+                }*/
+
+
+                //foreach (BusinessOwner businessOwner in allBusinessOwners)
+                //{
+                //    foreach (var groceryStore in allGroceryStores)
+                //    {
+                //        if (groceryStore.LicenseId == businessOwner.LicenseId)
+                //        {
+                //            groceryStoresOwners.AddRange(groceryStore.LicenseId, groceryStore);
+                //        }
+                //    }
+                //}
+                ViewData["businessOwners"] = businessOwnerList;
+            }
+        }
+    }
+}
