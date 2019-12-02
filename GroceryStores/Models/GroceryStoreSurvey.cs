532b819e4f59cd773508e2ec7233e8f8035f7dc8
EktaMahala <54607380+EktaMahala@users.noreply.github.com>
2019-12-02 14:16:11 -0500

    Class folders changes commit

---
+++ GroceryStores/Models/GroceryStoreSurvey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStores.Models
+{
+    public class GroceryStoreSurvey
+    {
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Mobile")]
+        public string mobile { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Display(Name = "Zip")]
+        public string zip { get; set; }
+        [Display(Name = "Most Preferred Grocery Stores to shop")]
+        public string stores { get; set; }
+        public string test { get; set; }
+
+    }
+}
